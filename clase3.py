f07368c2b269c248790b4a1e373aee2d393a7539
Ezurbriggen14 <eduz14@hotmail.com>
2024-02-15 16:58:19 -0300

    modificando entrada de numeros

--- clase3.py
+++ clase3.py
@@ -14,14 +14,18 @@ def programa():
     print("--------------")
 
     opcion = int(input("ingrese el numero 1 o 2 para realizar una operacion: "))
-    num1 = int(input("ingrese el primer numero entero: "))
-    num2 = int(input("ingrese el segundo numero entero: "))
+    
+    
 
     if(opcion == 1):
+        num1 = int(input("ingrese el primer numero entero: "))
+        num2 = int(input("ingrese el segundo numero entero: "))
         resultado = accion_a(num1, num2)
         print(f"el resultado es : {resultado}")
 
     elif(opcion == 2):
+        num1 = int(input("ingrese el primer numero entero: "))
+        num2 = int(input("ingrese el segundo numero entero: "))
         resultado = accion_b(num1, num2)
         print(f"el resultado es : {resultado}")
 
