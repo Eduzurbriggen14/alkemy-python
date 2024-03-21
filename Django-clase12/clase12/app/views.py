3a2da94fecac0c4b534ec003c3cc026ea0fc9017
Eduardo Zurbriggen <106617650+Eduzurbriggen14@users.noreply.github.com>
2024-03-21 16:48:48 -0300

    static y templates-clase 13

--- Django-clase12/clase12/app/views.py
+++ Django-clase12/clase12/app/views.py
@@ -2,21 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Template, Context
 
+
 def estudiante(request):
     estudiante_curso = [
-    {"nombre": "nicolas", "cursos": [{"titulo": "React", "descripcion": "Framework de Js"}, {"titulo": "Python", "descripcion": "Lenguaje"}]},
-    {"nombre": "carlos", "cursos": []},
-    {"nombre": "agostina", "cursos": [{"titulo": "Java", "descripcion": "Lenguaje"}, {"titulo": "Python", "descripcion": "Lenguaje"}, {"titulo": "Django", "descripcion": "Framework de Python"} ]}
-]
-    
+        {"nombre": "nicolas", "cursos": [{"titulo": "React", "descripcion": "Framework de Js"}, 
+                                         {"titulo": "Python", "descripcion": "Lenguaje"}]},
+
+        {"nombre": "carlos", "cursos": []},
+        
+        {"nombre": "agostina", "cursos": [{"titulo": "Python", "descripcion": "Lenguaje"},
+                                          {"titulo": "Django", "descripcion": "Framework de Python"}]}
+    ]
+
     ruta = "C:/Users/Gorila Games/Desktop/alkemy-python/Django-clase12/clase12/app/templates/index.html"
     archivo = open(ruta)
-
     contenido = archivo.read()
-
-    context =Context({"estudiantes": estudiante_curso}) 
-
     template = Template(contenido)
     archivo.close()
+    context = Context({"estudiantes": estudiante_curso})
 
     return HttpResponse(template.render(context))
